37c95b2adf13c8af20aebbe3ded28463a9e32553
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-02-06 20:47:38 +0100

    ajout command clear

---
+++ Cogs/moderation/clear.py
@@ -0,0 +1,20 @@
+import discord 
+from discord.ext import  commands 
+from discord import  app_commands
+from discord.ui import * 
+
+import logging 
+logger = logging.getLogger('discord.artichauds') 
+
+class clear(commands.Cog): 
+    def __init__(self,bot:commands.Bot) -> None: 
+        self.bot = bot 
+
+    @app_commands.command(name="clear",description="supprime un nombre donné de message")
+    @app_commands.checks.has_permissions(manage_messages=True)
+    @app_commands.rename(nb="nombre_de_messages")
+    @app_commands.describe(nb="nombre de message à supprimé")
+    async def clear(self,interaction:discord.Interaction,nb:int):
+        await interaction.response.defer(ephemeral=True)
+        await interaction.channel.purge(limit=nb)
+        await interaction.edit_original_response(content=f"j'ai supprimé *{nb}* messages")
