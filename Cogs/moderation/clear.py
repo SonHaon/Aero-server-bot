ade64bc8c27e0968d6bcd9feae29f0f39e29d952
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-02-07 17:05:57 +0100

    fix clear

--- Cogs/moderation/clear.py
+++ Cogs/moderation/clear.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import  commands 
 from discord import  app_commands
 from discord.ui import * 
+from ..checks import check
 
 import logging 
 logger = logging.getLogger('discord.artichauds') 
@@ -11,7 +12,7 @@ class clear(commands.Cog):
         self.bot = bot 
 
     @app_commands.command(name="clear",description="supprime un nombre donné de message")
-    @app_commands.checks.has_permissions(manage_messages=True)
+    @check.is_modo()
     @app_commands.rename(nb="nombre_de_messages")
     @app_commands.describe(nb="nombre de message à supprimé")
     async def clear(self,interaction:discord.Interaction,nb:int):
