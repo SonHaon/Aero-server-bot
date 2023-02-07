1baea510e09db54b3a1f20191468f6a04d08f608
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-02-07 17:10:48 +0100

    fix on_message_chtgpt

--- Cogs/chatgpt/event.py
+++ Cogs/chatgpt/event.py
@@ -10,8 +10,7 @@ class on_chatgpt_message(commands.Cog):
         self.bot = bot 
 
     @commands.Cog.listener(name="on_message")
-    @check.is_gpt_chan()
     async def on_message(self, message:discord.Message):
-        if message.author.bot == False:
+        if message.author.bot == False and message.channel.id == 1069338761772671158 :
             completion=openai.Completion.create(engine="text-davinci-003",prompt=message.content,max_tokens=2000-len(message.content))
             await message.reply(completion.choices[0].text)
