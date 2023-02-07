8534dd7fbfa36e8fe0571044de08b63ab0063bd4
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-02-07 14:27:11 +0100

    chanement import

--- Cogs/autre/ping.py
+++ Cogs/autre/ping.py
@@ -1,14 +1,7 @@
-import asyncio
-from datetime import datetime
 import discord
 from discord.ext import commands
 from discord import app_commands
 from discord.ui import *
-import random
-import os
-from io import BytesIO
-from PIL import Image,ImageFont,ImageDraw,ImageOps
-import logging
 
 class ping(commands.Cog):
     def __init__(self,bot:commands.Bot) -> None:
