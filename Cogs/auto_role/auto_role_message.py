a34eedfc7a13c254b98c5dda34a7fcc53a6bb13a
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-03-13 21:20:23 +0100

    premier test auto_role

--- Cogs/auto_role/auto_role_message.py
+++
@@ -1,9 +0,0 @@
-import asyncio 
-import discord 
-from discord.ext import  commands 
-from discord import  app_commands,ButtonStyle 
-from discord.ui import * 
-import random 
-
-from auto_role_buttons import *
-
