cfe153bb261d4c90b0dcbce0c6a95715bc92d6ae
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-03-13 21:27:26 +0100

    premier test auto_role

--- Cogs/auto_role/auto_role_buttons.py
+++ Cogs/auto_role/auto_role_buttons.py
@@ -10,7 +10,7 @@ class boutons_auto_role(discord.ui.View):
     @discord.ui.button(
         style=ButtonStyle.grey,
         emoji="<:bleu:1084924665954324491>",
-        custom_id="bleu",
+        custom_id="1",
         row=0
     )
     async def duo(self,interaction:discord.Interaction,button:discord.ui.Button):
@@ -19,7 +19,7 @@ class boutons_auto_role(discord.ui.View):
     @discord.ui.button(
         style=ButtonStyle.grey,
         emoji="<:jaune:1084924695838736404>",
-        custom_id="jaune",
+        custom_id="2",
         row=0
     )
     async def duel(self,interaction:discord.Interaction,button:discord.ui.Button):
@@ -28,7 +28,7 @@ class boutons_auto_role(discord.ui.View):
     @discord.ui.button(
         style=ButtonStyle.grey,
         emoji="<:orange:1084924723756027936>",
-        custom_id="orange",
+        custom_id="3",
         row=0
     )
     async def ios(self,interaction:discord.Interaction,button:discord.ui.Button):
@@ -37,7 +37,7 @@ class boutons_auto_role(discord.ui.View):
     @discord.ui.button(
         style=ButtonStyle.grey,
         emoji="<:rose:1084924591274741771>",
-        custom_id="rose",
+        custom_id="4",
         row=0
     )
     async def android(self,interaction:discord.Interaction,button:discord.ui.Button):
@@ -46,7 +46,7 @@ class boutons_auto_role(discord.ui.View):
     @discord.ui.button(
         style=ButtonStyle.grey,
         emoji="<:rouge:1084924751954329660>",
-        custom_id="rouge",
+        custom_id="5",
         row=1
     )
     async def infini(self,interaction:discord.Interaction,button:discord.ui.Button):
@@ -55,7 +55,7 @@ class boutons_auto_role(discord.ui.View):
     @discord.ui.button(
         style=ButtonStyle.grey,
         emoji="<:vert:1084924793016565830>",
-        custom_id="vert",
+        custom_id="6",
         row=1
     )
     async def alors(self,interaction:discord.Interaction,button:discord.ui.Button):
@@ -64,7 +64,7 @@ class boutons_auto_role(discord.ui.View):
     @discord.ui.button(
         style=ButtonStyle.grey,
         emoji="<:violet:1084924636971675791>",
-        custom_id="violet",
+        custom_id="7",
         row=1
     )
     async def alors(self,interaction:discord.Interaction,button:discord.ui.Button):
@@ -73,7 +73,7 @@ class boutons_auto_role(discord.ui.View):
     @discord.ui.button(
         style=ButtonStyle.grey,
         emoji="<:marron:1084929867985989703>",
-        custom_id="marron",
+        custom_id="8",
         row=1
     )
     async def alors(self,interaction:discord.Interaction,button:discord.ui.Button):
