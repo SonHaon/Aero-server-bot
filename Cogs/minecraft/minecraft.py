83bc3e96cd42b1e9008a6a3664d7060e44f609b2
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-07-13 12:37:39 +0200

    test minecraft commands

--- Cogs/minecraft/minecraft.py
+++ Cogs/minecraft/minecraft.py
@@ -14,7 +14,7 @@ class minecraft(commands.GroupCog, name="minecraft"):
     @check.is_SonHaon()
     async def commands(self, interaction:discord.Interaction, commande:str):
         await interaction.response.defer(ephemeral=True)
-        await interaction.edit_original_response(content=f"""la commandes `{commande}` à bien été executé\nretour\n```{os.popen(f'echo "{commande}" > /run/minecraft.stdin').read()}```""")
+        await interaction.edit_original_response(content=f"la commandes `{commande}` à bien été executé")
 
 
     @app_commands.command(name="start_mc",description="lance le serveur minecraft")
