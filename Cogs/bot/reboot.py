010313667ea9494e1011434b283a9c9d1900d089
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-07-13 13:23:44 +0200

    test minecraft commands

--- Cogs/bot/reboot.py
+++ Cogs/bot/reboot.py
@@ -17,7 +17,7 @@ class reboot(commands.Cog):
     @check.is_full_perm()
     async def reboot(self,interaction:discord.Interaction):
         await self.bot.change_presence(status=discord.Status.offline)
-        os.system("sudo systemctl restart Aero-server-bot")
         await interaction.response.send_message("le bot va redémarrer",ephemeral=True)
+        os.system("sudo systemctl restart Aero-server-bot")
         
         
