5d8a7e2fc16fdc4f45e22f62d2e377c3b3ed0b36
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-07-13 22:16:27 +0200

    test minecraft commands

--- Cogs/minecraft/minecraft.py
+++ Cogs/minecraft/minecraft.py
@@ -17,12 +17,19 @@ class minecraft(commands.GroupCog, name="minecraft"):
         await interaction.edit_original_response(content=f"la commandes `{commande}` à bien été executé")
 
 
-    @app_commands.command(name="start_mc",description="lance le serveur minecraft")
+    @app_commands.command(name="start",description="lance le serveur minecraft")
     @check.is_modo()
     async def start_mc(self, interaction:discord.Interaction):
         await interaction.response.defer(ephemeral=True)
         os.system(f'sudo systemctl start minecraft')
         await interaction.edit_original_response(content=f"le server démarre")
+
+    @app_commands.command(name="restart",description="relance le serveur minecraft")
+    @check.is_modo()
+    async def start_mc(self, interaction:discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
+        os.system(f'sudo systemctl restart minecraft')
+        await interaction.edit_original_response(content=f"le server redémarre")
 
 
     @app_commands.command(name="logs",description="montre les dernière ligne de la console")
