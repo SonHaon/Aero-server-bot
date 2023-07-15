3a90cb418c3940be1c061f390e5cf75ece97f3c1
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-07-15 10:35:52 +0200

    test logs

---
+++ Cogs/minecraft/logs.py
@@ -0,0 +1,16 @@
+import discord 
+from discord.ext import  commands 
+from discord.ui import * 
+from ..checks import check
+import openai
+
+
+class on_chatgpt_message(commands.Cog): 
+    def __init__(self,bot:commands.Bot) -> None: 
+        self.bot = bot 
+
+    @commands.Cog.listener(name="on_message")
+    async def on_message(self, message:discord.Message):
+        if message.channel.id == 1050457771537080410 and message.content.lower() == "log" or message.content.lower() == "logs":
+            message.channel.send(file=discord.File(r"/home/sonhaon/minecraft/logs/latest.log"))
+            
