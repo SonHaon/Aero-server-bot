0206637f20d5e054f0db10911bd73aea04dd994f
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-07-15 10:38:01 +0200

    test logs

--- Cogs/minecraft/logs.py
+++ Cogs/minecraft/logs.py
@@ -11,6 +11,6 @@ class logs(commands.Cog):
 
     @commands.Cog.listener(name="on_message")
     async def logs(self, message:discord.Message):
-        if message.channel.id == 1050457771537080410 and message.content.lower() == "log" or message.content.lower() == "logs":
+        if message.channel.id == 1050457771537080410 and (message.content.lower() == "log" or message.content.lower() == "logs"):
             message.channel.send(file=discord.File(r"/home/sonhaon/minecraft/logs/latest.log"))
             
