838efc82aa807f1d9cf991fa7f7479d48f602124
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-07-15 10:52:12 +0200

    test logs

--- Cogs/minecraft/logs.py
+++ Cogs/minecraft/logs.py
@@ -12,5 +12,18 @@ class logs(commands.Cog):
     @commands.Cog.listener(name="on_message")
     async def logs(self, message:discord.Message):
         if message.channel.id == 1050457771537080410 and (message.content.lower() == "log" or message.content.lower() == "logs"):
-            await message.channel.send(file=discord.File(r"/home/sonhaon/minecraft/logs/latest.log"))
-            
+            if message.content.endswith("file"):
+                await message.channel.send(file=discord.File(r"/home/sonhaon/minecraft/logs/latest.log"))
+            else:
+                with open("/home/sonhaon/minecraft/logs/latest.log","r",encoding="utf8") as file:
+                    lines = file.readlines()
+                lines.reverse()
+                log=[]
+                for i in range(100):
+                    log.append(lines[i])
+                    if "".join(log).count("") >= 2000:
+                        log.pop()
+                        break
+                log.reverse()
+                await message.channel.send(content=f"```{''.join(log)}```")    
+            await message.delete      
