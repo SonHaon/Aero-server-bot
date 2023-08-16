20ee0b440a2e9dde6eb0c6a6188a7f66f966e605
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-08-16 15:53:07 +0200

    fix commande dm

--- Cogs/autre/dm.py
+++ Cogs/autre/dm.py
@@ -13,6 +13,8 @@ class dm(commands.Cog):
     @check.is_SonHaon()
     async def dm(self,interaction:discord.Interaction,user:discord.User,message:str):
         await interaction.response.defer(ephemeral=True)
-        dm = await user.create_dm()
-        await dm.send(message)
-        await interaction.edit_original_response(content=f"j'ai bien envoyé `{message}` à {user.mention}")
+        try:
+            await user.send(message)
+            await interaction.edit_original_response(content=f"j'ai bien envoyé `{message}` à {user.mention}")
+        except:
+            await interaction.edit_original_response(content=f"je n'ai pas pu envoyé de message à {user.mention}, soit nous n'avons pas de server en commun soit il n'accepte pas les messages privés")
