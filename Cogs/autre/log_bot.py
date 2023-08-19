2ee648f137ef3361a8580634192c0e1278c00836
SonHaon <97341224+SonHaon@users.noreply.github.com>
2023-08-19 11:12:15 +0200

    edit log_bot test

--- Cogs/autre/log_bot.py
+++ Cogs/autre/log_bot.py
@@ -10,9 +10,9 @@ class log_bot(commands.Cog):
     def __init__(self,bot:commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name="lob_bot",description="fait des test")
+    @app_commands.command(name="log_bot",description="fait des test")
     @check.is_SonHaon()
-    async def ping(self,interaction:discord.Interaction):
+    async def log_bot(self,interaction:discord.Interaction):
         await interaction.response.defer(ephemeral=True)
         message=os.popen("ping 192.168.1.34").read()
         await interaction.edit_original_response(content=message)
